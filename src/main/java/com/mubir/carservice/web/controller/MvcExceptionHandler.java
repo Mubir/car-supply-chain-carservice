088c6e5f771654925e69bf558d8f9c693fc1a6a1
Munir <info.mubir@gmail.com>
2020-07-25 12:12:15 +0900

    controller global error handler added

--- src/main/java/com/mubir/carservice/web/controller/MvcExceptionHandler.java
+++ src/main/java/com/mubir/carservice/web/controller/MvcExceptionHandler.java
@@ -1,4 +1,24 @@
 package com.mubir.carservice.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
 public class MvcExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> vErrorHandler(ConstraintViolationException e)
+    {
+        List<String> error = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(constrainViolation -> {
+            error.add(constrainViolation.getPropertyPath()+" : "+constrainViolation.getMessage());
+        });
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
