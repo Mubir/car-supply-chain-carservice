06e99d22b46debdd961ae2a2cedbe42d7f6c2732
Munir <info.mubir@gmail.com>
2020-08-08 13:43:11 +0900

    data init by code added as data.sql not work

--- src/main/java/com/mubir/carservice/bootstrap/CarLoader.java
+++ src/main/java/com/mubir/carservice/bootstrap/CarLoader.java
@@ -2,12 +2,15 @@ package com.mubir.carservice.bootstrap;
 
 import com.mubir.carservice.domain.Car;
 import com.mubir.carservice.repositories.CarRepository;
+import com.mubir.carservice.web.model.CarModelEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-//@Component
+@Component
+@RequiredArgsConstructor
 public class CarLoader implements CommandLineRunner
 {
     public static final String CAR_1_UPC = "0631234200036";
@@ -15,32 +18,46 @@ public class CarLoader implements CommandLineRunner
     public static final String CAR_3_UPC = "0083783375213";
     private final CarRepository carRepository;
 
-    public CarLoader(CarRepository carRepository) {
-        this.carRepository = carRepository;
-    }
-
     @Override
     public void run(String... args) throws Exception {
-        generateSeedvalue();
+        if(carRepository.count()==0) {
+            generateSeedvalue();
+        }
     }
 
     private void generateSeedvalue() {
-        if(carRepository.count()<=0)
-        {
-            carRepository.save(Car.builder()
-                    .carModel("xzy")
-                    .carName("BMW")
+
+
+            Car c1=Car.builder()
+                    .carModel(CarModelEnum.NISSAN.name())
+                    .carName("T1z")
                     .upc(CAR_1_UPC)
                     .price(new BigDecimal("50000"))
-                    .build());
+                    .minOnHand(66)
+                    .quantityPreOrder(88)
+                    .build();
 
-            carRepository.save(Car.builder()
-                    .carModel("xoxo")
-                    .carName("TOYOTA")
+            Car c2=Car.builder()
+                    .carModel(CarModelEnum.TOYOTA.name())
+                    .carName("T-sytle")
                     .upc(CAR_2_UPC)
                     .price(new BigDecimal("50000"))
-                    .build());
-        }
+                    .minOnHand(123)
+                    .quantityPreOrder(23)
+                    .build();
+            Car c3=Car.builder()
+                    .carModel(CarModelEnum.BMW.name())
+                    .carName("S-cube")
+                    .upc(CAR_3_UPC)
+                    .price(new BigDecimal("50000"))
+                    .minOnHand(3)
+                    .quantityPreOrder(6)
+                    .build();
+
+        carRepository.save(c1);
+        carRepository.save(c2);
+        carRepository.save(c3);
+
         System.out.println(" $$$$$$$$$$$ "+carRepository.findAll().toString());
     }
 }
